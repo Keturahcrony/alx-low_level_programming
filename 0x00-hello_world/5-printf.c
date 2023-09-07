600e34d343c9a1ed5a38e0bd27c8a33dc3f645d0
keturah crony <keturahcrony@gmail.com>
2023-09-07 06:57:44 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main fucntion of the program
+ * printf - the standard output
+ * Return: the return value is 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
