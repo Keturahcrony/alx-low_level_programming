41add634134ab014aeb5367bb47a33b69b1c5569
keturah crony <keturahcrony@gmail.com>
2023-09-08 09:54:00 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * putchar - output function
+ * Return: returning value
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
