9697827f6ec3830bd396981cb0f350958cbe2c9f
keturah crony <keturahcrony@gmail.com>
2023-09-08 08:53:59 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * putchar - display output
+ * Return: returning  value
+ */
+
+int main(void)
+{
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar("\n");
+}
