061f070b4fd4ca8bb9ffc3d4931355cdb499939b
keturah crony <keturahcrony@gmail.com>
2023-09-08 14:59:49 -0700

    print combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int i = '0';
-	int j = '0';
+	int j = '1';
 
 	while (i <= '9')
 	{
@@ -31,7 +31,7 @@ int main(void)
 			}
 			j++;
 		}
-		j = '0';
+		j = '1';
 			i++;
 	}
 	return (0);
