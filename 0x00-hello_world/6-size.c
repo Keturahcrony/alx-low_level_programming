9237a42295029923b7b0c79666e96eff2a309942
keturah crony <keturahcrony@gmail.com>
2023-09-11 01:03:00 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,8 @@ int main(void)
 					(unsigned long)sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n",
 			(unsigned long)sizeof(long long int));
-			printf("Size of a float: %lu byte(s)\n",
+			printf("Size of a float: %lu byte(s)",
 					(unsigned long)sizeof(float));
+			printf("[stderr]: [Anything]");
 	return (0);
 }
