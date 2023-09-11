004f99e45694907e33ae0067a34a550dc8ce9895
keturah crony <keturahcrony@gmail.com>
2023-09-11 00:52:28 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,9 +3,10 @@
 /**
  * main - the main body
  *
- * printf - text to display inoutput
+ * printf - text to display output
  * Return: the value of 0
  */
+
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
@@ -14,7 +15,7 @@ int main(void)
 					(unsigned long)sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n",
 			(unsigned long)sizeof(long long int));
-			printf("Size of a float: %lu byte(s)",
+			printf("Size of a float: %lu byte(s)\n",
 					(unsigned long)sizeof(float));
 	return (0);
 }
