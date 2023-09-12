b5a8e47859e7b897fb198e48b7883edfc11744fa
keturah crony <keturahcrony@gmail.com>
2023-09-12 09:45:10 -0700

    lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,11 @@
  * Return: return alphabet
  */
 
-int main(void)
+void print_alphabet(void)
 {
 	char letter;
 
 		for (letter = 'a'; letter <= 'z'; letter++)
 			putchar(letter);
 		putchar('\n');
-		return (0);
 }
