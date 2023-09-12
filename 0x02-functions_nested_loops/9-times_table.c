caa57a694cb4324a703e10dbb4b943931f861655
keturah crony <keturahcrony@gmail.com>
2023-09-12 13:32:16 -0700

    corrections

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,12 +22,12 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
 				_putchar (k + '0');
 			}
 			else if (k > 10)
 			{
 				_putcher(',');
-				_putcher(' ');
 				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
