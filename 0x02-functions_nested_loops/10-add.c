cf7787a6571b82036086ee04eec37eb0524cba5e
keturah crony <keturahcrony@gmail.com>
2023-09-12 13:24:48 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two digits
+ * @a: int to be added to int b
+ * @b: int to be added to int a
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int c;
+	c = a + b;
+	return (c);
+}
