b98a9ae90e113684b28285ff4e06e30935c9a926
keturah crony <keturahcrony@gmail.com>
2023-09-13 14:50:50 -0700

    _islower

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
-	int _islower(int c);
-	int c;
+	int r;
 
-	c = _islower('H');
-	_putchar(c + '0');
-	c = _islower('o');
-	_putchar(c + '0');
-	c = _islower(108);
-	_putchar(c + '0');
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
 	_putchar('\n');
 	return (0);
 }
