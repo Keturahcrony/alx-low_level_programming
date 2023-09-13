167c747ce617b037cb0685c034f23fff78765b21
keturah crony <keturahcrony@gmail.com>
2023-09-13 13:48:19 -0700

    largest numbers

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,13 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
-#include "main.h"
-
-void positive_or_negative(int i)
-{
-	if (i > 0)
-		printf("%d is positive\n", i);
-	else if (i == 0)
-		printf("%d is 0\n"' i);
-	else
-		printf("%d is negative\n", i);
-	return (0);
-}
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif
